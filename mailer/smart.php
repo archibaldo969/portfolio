2223f2bcf2bff9e7b14b30a9e2512ff91a884abe
archibaldo969 <72183023+archibaldo969@users.noreply.github.com>
2021-05-04 16:10:53 +0600

    Add files via upload

---
+++ mailer/smart.php
@@ -0,0 +1,44 @@
+<?php 
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'artur.sidorenko.02@bk.ru';                 // Наш логин
+$mail->Password = '02072002Art';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('Artur', 'Pulse');   // От кого письмо 
+$mail->addAddress('artur.sidorenko.02@mail.ru');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Данные';
+$mail->Body    = '
+		Пользователь оставил данные <br> 
+	Имя: ' . $name . ' <br>
+	E-mail: ' . $email . ''; '<br>
+	Сообщение: ' . $message .
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
